360fc078990b8fee4a7cf0238f7d3437f9274ece
Zac Bowhay <zbowhay@kou.pn>
2021-09-19 21:54:02 -0600

    BoardTile state lifted up to Board

--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -1,29 +1,52 @@
-import BoardTile from './BoardTile';
-import './Board.css';
+import React from 'react';
 import { getIcons } from './Icons';
+import BoardTile from './BoardTile';
+import { BoardTileProps } from './BoardTile';
+import './Board.css';
 
-function Board() {
-    const difficulty = {
-        easy: 4*4,
-        medium: 6*6,
-        hard: 8*8
-    };
-    const currentDifficulty = difficulty.medium;
-    const icons = getIcons(currentDifficulty);
-    const classes = `boardContainer ${Object.entries(difficulty).find(val => val[1] === currentDifficulty)?.[0]}`;
 
-    const tileClicked = (i: any) => {
-        console.log('clicked', i);
+
+interface BoardState {
+    difficulty: number; // current difficulty
+    tileProps: BoardTileProps[];
+}
+class Board extends React.Component<{}, BoardState> {
+    difficulty = { easy: 4*4, medium: 6*6, hard: 8*8 };
+
+    constructor(props: {}) {
+        super(props);
+        this.state = {
+            difficulty: this.difficulty.medium,
+            tileProps: getIcons(this.difficulty.medium).map((icon, i) => ({ icon, i, flipped: false, clickHandler: this.tileClicked }))
+        };
     }
 
-    const tiles = icons.map((val, i) =>
-        <BoardTile i={i} icon={val} flipped={false} clickHandler={tileClicked}></BoardTile>
-    );
-    return (
-        <div className={classes}>
-            {tiles}
-        </div>
-    );
+    tileClicked = (tileIndex: number) => {
+        console.log(`tile ${tileIndex} clicked!`);
+
+        this.setState((prev) => ({
+            difficulty: prev.difficulty,
+            tileProps: prev.tileProps.map((val, i) => {
+                if (i === tileIndex) {
+                    return {...val, ...{ flipped: !val.flipped }};
+                }
+                return val;
+            })
+        }));
+    }
+
+    render() {
+        const classes = `boardContainer ${Object.entries(this.difficulty).find(val => val[1] === this.state.difficulty)?.[0]}`;
+        return (
+            <div className={classes}>
+                {this.state.tileProps.map(p => {
+                    return (
+                        <BoardTile {...p}></BoardTile>
+                    )
+                })}
+            </div>
+        );
+    }
 }
 
 export default Board;
--- src/components/board/BoardTile.tsx
+++ src/components/board/BoardTile.tsx
@@ -2,38 +2,24 @@ import { Card } from "@mui/material";
 import React from "react";
 import './BoardTile.css';
 
-interface BoardTileProps {
+export interface BoardTileProps {
     i: number;
-    flipped?: boolean;
+    flipped: boolean;
     icon: any;
-    clickHandler?: any;
+    clickHandler: any;
 }
 
 interface BoardTileState {
     flipped: boolean;
 }
 class BoardTile extends React.Component<BoardTileProps, BoardTileState> {
-    classes: any;
-
-    constructor(props: BoardTileProps) {
-        super(props);
-        this.state = { flipped: false };
-    }
-    
-    handleClick = (e: any) => {
-        console.log('hello', e);
-        this.setState(prev => ({
-            flipped: !prev.flipped
-        }))
-    }
-    
     render() {
         return (
             <Card key={this.props.i}
-                className={`boardTile ${this.state.flipped ? 'cardBack' : 'cardFront'}`}
-                onClick={this.handleClick}
-            >
-                {this.state.flipped && <this.props.icon className='cardIcon'></this.props.icon>}
+                className={`boardTile ${this.props.flipped ? 'cardBack' : 'cardFront'}`}
+                onClick={(e) => this.props.clickHandler(this.props.i, e)}
+                >
+                {this.props.flipped && <this.props.icon className='cardIcon'></this.props.icon>}
             </Card>
         );
     }
